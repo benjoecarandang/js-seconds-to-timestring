a1f065008075a82a8bb4c99e704f087a0ecb1aa6
Benjoe Oates Carandang <benjoe.carandang@gmail.com>
2022-08-05 08:17:42 +0800

    initial commit for seconds to timestring

---
+++ app.js
@@ -0,0 +1,48 @@
+function secondsToTime(inputSeconds) {
+    const secondsInAMinute = 60;
+    const secondsInAnHour = 60 * secondsInAMinute;
+    const secondsInADay = 24 * secondsInAnHour;
+    const secondsInAweek = secondsInADay * 7;
+    const secondsInAmonth = secondsInADay * 31;
+
+    // Extract months
+    const months = Math.floor(inputSeconds / secondsInAmonth);
+    
+    // Extract weeks
+    const weeks = Math.floor(inputSeconds/ secondsInAweek);
+    
+    // Extract days
+    const days = Math.floor(inputSeconds / secondsInADay) % 7;
+    
+    // Extract hours
+    const hourSeconds = inputSeconds % secondsInADay;
+    const hours = Math.floor(hourSeconds / secondsInAnHour);
+
+    // Extract minutes
+    const minuteSeconds = hourSeconds % secondsInAnHour;
+    const minutes = Math.floor(minuteSeconds / secondsInAMinute);
+
+    // Extract the remaining seconds
+    const remainingSeconds = minuteSeconds % secondsInAMinute;
+    const seconds = Math.ceil(remainingSeconds);
+
+    // Format and return
+    const timeParts = [];
+    const sections = {
+        month: +months,
+        week: +weeks,
+        day: +days,
+        hour: +hours,
+        minute: +minutes,
+        second: +seconds,
+    };
+    
+    for (const key in sections) {
+      const value = sections[key];
+      timeParts.push(`${value} ${key}${value == 1 ? '' : 's'}`);
+    }
+
+    return timeParts.join(', ');
+}
+
+console.log(secondsToTime(10000000));
